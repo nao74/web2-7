c166a0819815a2010a7f75cb51bb937d7e737464
nao74 <38421007+nao74@users.noreply.github.com>
2018-11-12 17:58:12 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,23 @@
 
-window.alert('こんにちは世界');
+var number=Math.floor(Math.random()*3);
+var answer=parseInt(window.prompt('数あてゲーム 0から2の数字を入力してください'));
 
-console.log('こんにちは');
+var message;
+if(answer===number)
+{
+  message='あたり';
+}
+else if(answer<number)
+{
+  message='残念　もっと大きいです';
+}
+else if(number<answer)
+{
+  message='残念　もっと小さいです';
+}
+else
+{
+  message='0から3の数字を入力してください';
+}
+
+document.getElementById('choice').textContent=message;
